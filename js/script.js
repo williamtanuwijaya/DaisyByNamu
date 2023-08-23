0bbece74ae80aa262aeeab8d0427689e112c698a
williamtanuwijaya <will.tw0807@gmail.com>
2023-08-24 01:23:44 +0700

    make a landing page DaisyByNamu

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//Toggle class active
+
+const navbarNav = document.querySelector('.navbar-nav');
+// when the hamburger menu is clicked
+
+document.querySelector('#hamburger-menu').onclick = () =>{
+  navbarNav.classList.toggle('active');
+};
+
+//Klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+    navbarNav.classList.remove('active');
+  }
+})
