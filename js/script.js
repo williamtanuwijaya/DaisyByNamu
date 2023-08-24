1638308bdde8805838d03c4097bdcac33db7d587
williamtanuwijaya <will.tw0807@gmail.com>
2023-08-24 22:35:58 +0700

    update

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ document.querySelector('#hamburger-menu').onclick = () =>{
 //Klik di luar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector('#hamburger-menu');
 
-// document.addEventListener('click', function(e){
-//   if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
-//     navbarNav.classList.remove('active');
-//   }
-// })
+document.addEventListener('click', function(e){
+  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+    navbarNav.classList.remove('active');
+  }
+})
