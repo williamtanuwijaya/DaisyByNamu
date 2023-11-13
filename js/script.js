0daa2cf85904e0796772098d1d3ac7150551f7b2
williamtanuwijaya <will.tw0807@gmail.com>
2023-11-13 13:25:33 +0700

    update

--- js/script.js
+++ js/script.js
@@ -9,37 +9,37 @@ document.querySelector('#hamburger-menu').onclick = (e) => {
 };
 
 //Toggle class active untuk search form
-const searchForm = document.querySelector('.search-form');
-const searchBox = document.querySelector('#search-box');
+// const searchForm = document.querySelector('.search-form');
+// const searchBox = document.querySelector('#search-box');
 
-document.querySelector('#search-button').onclick = (e) => {
-  searchForm.classList.toggle('active');
-  searchBox.focus();
-  e.preventDefault();
-};
+// document.querySelector('#search-button').onclick = (e) => {
+//   searchForm.classList.toggle('active');
+//   searchBox.focus();
+//   e.preventDefault();
+// };
 
 // Tonggle class active untuk shopping cart
-const shoppingCart = document.querySelector('.shopping-cart');
-document.querySelector('#shopping-cart-button').onclick = () => {
-  shoppingCart.classList.toggle('active');
-  e.preventDefault();
-};
+// const shoppingCart = document.querySelector('.shopping-cart');
+// document.querySelector('#shopping-cart-button').onclick = () => {
+//   shoppingCart.classList.toggle('active');
+//   e.preventDefault();
+// };
 
 //Klik di luar element
 const hm = document.querySelector('#hamburger-menu');
-const sb = document.querySelector('#search-button');
-const sc = document.querySelector('#shopping-cart-button');
+// const sb = document.querySelector('#search-button');
+// const sc = document.querySelector('#shopping-cart-button');
 
 document.addEventListener('click', function (e) {
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
   }
-  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
-    searchForm.classList.remove('active');
-  }
-  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
-    shoppingCart.classList.remove('active');
-  }
+  // if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+  //   searchForm.classList.remove('active');
+  // }
+  // if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+  //   shoppingCart.classList.remove('active');
+  // }
 });
 
 // Modal Box
